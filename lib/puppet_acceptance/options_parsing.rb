7882fb14ff62dd26970016ac34dfd71190cb78c5
Justin Stoller <justin@puppetlabs.com>
2013-07-11 12:24:50 -0700

    Extract out option defaults into centralized constant

--- lib/puppet_acceptance/options_parsing.rb
+++ lib/puppet_acceptance/options_parsing.rb
@@ -1,26 +1,52 @@
-module PuppetAcceptance
-  class Options
-    GITREPO = 'git://github.com/puppetlabs'
+module puppetacceptance
+  class options
+    gitrepo = 'git://github.com/puppetlabs'
+
+    DEFAULTS = {
+      :config => nil,
+      :options_file => nil,
+      :type => 'pe',
+      :helper => [],
+      :load_path => [],
+      :tests => [],
+      :pre_suite => [],
+      :post_suite => [],
+      :provision => true,
+      :preserve_hosts => false,
+      :root_keys => false,
+      :keyfile => "#{env['home']}/.ssh/id_rsa",
+      :install => [],
+      :modules => [],
+      :quiet => false,
+      :xml => false,
+      :color => true,
+      :debug => false,
+      :dry_run => false,
+      :fail_mode => nil,
+      :timesync => false,
+      :repo_proxy => false,
+      :add_el_extras => false
+    }
 
     def self.options
       return @options
     end
 
     def self.repo?
-      GITREPO
+      gitrepo
     end
 
     def self.parse_install_options(install_opts)
       install_opts.map! { |opt|
         case opt
-          when /^PUPPET\//
-            opt = "#{GITREPO}/puppet.git##{opt.split('/', 2)[1]}"
-          when /^FACTER\//
-            opt = "#{GITREPO}/facter.git##{opt.split('/', 2)[1]}"
-          when /^HIERA\//
-            opt = "#{GITREPO}/hiera.git##{opt.split('/', 2)[1]}"
-          when /^HIERA-PUPPET\//
-            opt = "#{GITREPO}/hiera-puppet.git##{opt.split('/', 2)[1]}"
+          when /^puppet\//
+            opt = "#{gitrepo}/puppet.git##{opt.split('/', 2)[1]}"
+          when /^facter\//
+            opt = "#{gitrepo}/facter.git##{opt.split('/', 2)[1]}"
+          when /^hiera\//
+            opt = "#{gitrepo}/hiera.git##{opt.split('/', 2)[1]}"
+          when /^hiera-puppet\//
+            opt = "#{gitrepo}/hiera-puppet.git##{opt.split('/', 2)[1]}"
         end
         opt
       }
@@ -29,7 +55,7 @@ module PuppetAcceptance
 
     def self.file_list(paths)
       files = []
-      if not paths.empty? 
+      if not paths.empty?
         paths.each do |root|
           if File.file? root then
             files << root
@@ -38,7 +64,7 @@ module PuppetAcceptance
               File.join(root, "**/*.rb")
             ).select { |f| File.file?(f) }
             if discover_files.empty?
-              raise ArgumentError, "Empty directory used as an option (#{root})!"
+              raise ArgumentError, "empty directory used as an option (#{root})!"
             end
             files += discover_files
           end
@@ -50,44 +76,39 @@ module PuppetAcceptance
     def self.parse_args
       return @options if @options
 
-      @no_args = ARGV.empty? ? true : false
+      @no_args = argv.empty? ? true : false
 
-      @defaults = {}
       @options = {}
       @options_from_file = {}
 
       optparse = OptionParser.new do|opts|
-        # Set a banner
-        opts.banner = "Usage: #{File.basename($0)} [options...]"
+        # set a banner
+        opts.banner = "usage: #{file.basename($0)} [options...]"
 
-        @defaults[:config] = nil
-        opts.on '-c', '--config FILE',
-                'Use configuration FILE' do |file|
+        opts.on '-c', '--config file',
+                'use configuration file' do |file|
           @options[:config] = file
         end
 
-        @defaults[:options_file] = nil
-        opts.on '-o', '--options-file FILE',
-                'Read options from FILE',
-                'This should evaluate to a ruby hash.',
-                'CLI optons are given precedence.' do |file|
+        opts.on '-o', '--options-file file',
+                'read options from file',
+                'this should evaluate to a ruby hash.',
+                'cli optons are given precedence.' do |file|
           @options_from_file = parse_options_file file
         end
 
-        @defaults[:type] = 'pe'
-        opts.on '--type TYPE',
+        opts.on '--type type',
                 'one of git or pe', 
                 'used to determine underlying path structure of puppet install',
                 'defaults to pe' do |type|
           @options[:type] = type
         end
 
-        @defaults[:helper] = []
-        opts.on '--helper PATH/TO/SCRIPT',
-                'Ruby file evaluated prior to tests',
+        opts.on '--helper path/to/script',
+                'ruby file evaluated prior to tests',
                 '(a la spec_helper)' do |script|
           @options[:helper] = []
-          if script.is_a?(Array)
+          if script.is_a?(array)
             @options[:helper] += script
           elsif script =~ /,/
             @options[:helper] += script.split(',')
@@ -96,11 +117,10 @@ module PuppetAcceptance
           end
         end
 
-        @defaults[:load_path] = []
-        opts.on  '--load-path /PATH/TO/DIR,/ADDITIONAL/DIR/PATHS',
-                 'Add paths to LOAD_PATH'  do |value|
+        opts.on  '--load-path /path/to/dir,/additional/dir/paths',
+                 'add paths to load_path'  do |value|
           @options[:load_path] = []
-          if value.is_a?(Array)
+          if value.is_a?(array)
             @options[:load_path] += value
           elsif value =~ /,/
             @options[:load_path] += value.split(',')
@@ -109,11 +129,10 @@ module PuppetAcceptance
           end
         end
 
-        @defaults[:tests] = []
-        opts.on  '-t', '--tests /PATH/TO/DIR,/ADDITIONA/DIR/PATHS,/PATH/TO/FILE.rb',
-                 'Execute tests from paths and files' do |value|
+        opts.on  '-t', '--tests /path/to/dir,/additiona/dir/paths,/path/to/file.rb',
+                 'execute tests from paths and files' do |value|
           @options[:tests] = []
-          if value.is_a?(Array)
+          if value.is_a?(array)
             @options[:tests] += value
           elsif value =~ /,/
             @options[:tests] += value.split(',')
@@ -122,15 +141,14 @@ module PuppetAcceptance
           end
           @options[:tests] = file_list(@options[:tests])
           if @options[:tests].empty?
-            raise ArgumentError, "No tests to run!"
+            raise ArgumentError, "no tests to run!"
           end
         end
 
-        @defaults[:pre_suite] = []
-        opts.on '--pre-suite /PRE-SUITE/DIR/PATH,/ADDITIONAL/DIR/PATHS,/PATH/TO/FILE.rb',
-                'Path to project specific steps to be run BEFORE testing' do |value|
+        opts.on '--pre-suite /pre-suite/dir/path,/additional/dir/paths,/path/to/file.rb',
+                'path to project specific steps to be run before testing' do |value|
           @options[:pre_suite] = []
-          if value.is_a?(Array)
+          if value.is_a?(array)
             @options[:pre_suite] += value
           elsif value =~ /,/
             @options[:pre_suite] += value.split(',')
@@ -139,15 +157,14 @@ module PuppetAcceptance
           end
           @options[:pre_suite] = file_list(@options[:pre_suite])
           if @options[:pre_suite].empty?
-            raise ArgumentError, "Empty pre-suite!"
+            raise ArgumentError, "empty pre-suite!"
           end
         end
 
-        @defaults[:post_suite] = []
-        opts.on '--post-suite /POST-SUITE/DIR/PATH,/OPTIONAL/ADDITONAL/DIR/PATHS,/PATH/TO/FILE.rb',
-                'Path to project specific steps to be run AFTER testing' do |value|
+        opts.on '--post-suite /post-suite/dir/path,/optional/additonal/dir/paths,/path/to/file.rb',
+                'path to project specific steps to be run after testing' do |value|
           @options[:post_suite] = []
-          if value.is_a?(Array)
+          if value.is_a?(array)
             @options[:post_suite] += value
           elsif value =~ /,/
             @options[:post_suite] += value.split(',')
@@ -156,45 +173,40 @@ module PuppetAcceptance
           end
           @options[:post_suite] = file_list(@options[:post_suite])
           if @options[:post_suite].empty?
-            raise ArgumentError, "Empty post-suite!"
+            raise ArgumentError, "empty post-suite!"
           end
         end
 
-        @defaults[:provision] = true
         opts.on '--[no-]provision',
-                'Do not provision vm images before testing',
+                'do not provision vm images before testing',
                 '(default: true)' do |bool|
           @options[:provision] = bool
         end
 
-        @defaults[:preserve_hosts] = false
         opts.on '--[no-]preserve-hosts',
-                'Preserve cloud instances' do |value|
+                'preserve cloud instances' do |value|
           @options[:preserve_hosts] = value
         end
 
-        @defaults[:root_keys] = false
         opts.on '--root-keys',
-                'Install puppetlabs pubkeys for superuser',
+                'install puppetlabs pubkeys for superuser',
                 '(default: false)' do |bool|
           @options[:root_keys] = bool
         end
 
-        @defaults[:keyfile] = "#{ENV['HOME']}/.ssh/id_rsa"
-        opts.on '--keyfile /PATH/TO/SSH/KEY',
-                'Specify alternate SSH key',
+        opts.on '--keyfile /path/to/ssh/key',
+                'specify alternate ssh key',
                 '(default: ~/.ssh/id_rsa)' do |key|
           @options[:keyfile] = key
         end
 
 
-        @defaults[:install] = []
-        opts.on '-i URI', '--install URI',
-                'Install a project repo/app on the SUTs', 
-                'Provide full git URI or use short form KEYWORD/name',
-                'supported keywords: PUPPET, FACTER, HIERA, HIERA-PUPPET' do |value|
+        opts.on '-i uri', '--install uri',
+                'install a project repo/app on the suts', 
+                'provide full git uri or use short form keyword/name',
+                'supported keywords: puppet, facter, hiera, hiera-puppet' do |value|
           @options[:install] = []
-          if value.is_a?(Array)
+          if value.is_a?(array)
             @options[:install] += value
           elsif value =~ /,/
             @options[:install] += value.split(',')
@@ -204,79 +216,69 @@ module PuppetAcceptance
           @options[:install] = parse_install_options(@options[:install])
         end
 
-        @defaults[:modules] = []
-        opts.on('-m', '--modules URI', 'Select puppet module git install URI') do |value|
+        opts.on('-m', '--modules uri', 'select puppet module git install uri') do |value|
           @options[:modules] ||= []
           @options[:modules] << value
         end
 
-        @defaults[:quiet] = false
         opts.on '-q', '--[no-]quiet',
-                'Do not log output to STDOUT',
+                'do not log output to stdout',
                 '(default: false)' do |bool|
           @options[:quiet] = bool
         end
 
-        @defaults[:xml] = false
         opts.on '-x', '--[no-]xml',
-                'Emit JUnit XML reports on tests',
+                'emit junit xml reports on tests',
                 '(default: false)' do |bool|
           @options[:xml] = bool
         end
 
-        @defaults[:color] = true
         opts.on '--[no-]color',
-                'Do not display color in log output',
+                'do not display color in log output',
                 '(default: true)' do |bool|
           @options[:color] = bool
         end
 
-        @defaults[:debug] = false
         opts.on '--[no-]debug',
-                'Enable full debugging',
+                'enable full debugging',
                 '(default: false)' do |bool|
           @options[:debug] = bool
         end
 
-        @defaults[:dry_run] = false
         opts.on  '-d', '--[no-]dry-run',
-                 'Report what would happen on targets',
+                 'report what would happen on targets',
                  '(default: false)' do |bool|
           @options[:dry_run] = bool
           $dry_run = bool
         end
 
-        @defaults[:fail_mode] = nil
-        opts.on '--fail-mode [MODE]',
-                'How should the harness react to errors/failures',
-                'Possible values:',
+        opts.on '--fail-mode [mode]',
+                'how should the harness react to errors/failures',
+                'possible values:',
                 'fast (skip all subsequent tests, cleanup, exit)',
                 'stop (skip all subsequent tests, do no cleanup, exit immediately)'  do |mode|
           @options[:fail_mode] = mode
         end
 
-        @defaults[:timesync] = false
         opts.on '--[no-]ntp',
-                'Sync time on SUTs before testing',
+                'sync time on suts before testing',
                 '(default: false)' do |bool|
           @options[:timesync] = bool
         end
 
-        @defaults[:repo_proxy] = false
         opts.on '--repo-proxy',
-                'Proxy packaging repositories on ubuntu, debian and solaris-11',
+                'proxy packaging repositories on ubuntu, debian and solaris-11',
                 '(default: false)' do
           @options[:repo_proxy] = true
         end
 
-        @defaults[:add_el_extras] = false
         opts.on '--add-el-extras',
-                'Add Extra Packages for Enterprise Linux (EPEL) repository to el-* hosts',
+                'add extra packages for enterprise linux (epel) repository to el-* hosts',
                 '(default: false)' do
           @options[:add_el_extras] = true
         end
 
-        opts.on('--help', 'Display this screen' ) do |yes|
+        opts.on('--help', 'display this screen' ) do |yes|
           puts opts
           exit
         end
@@ -284,15 +286,15 @@ module PuppetAcceptance
 
       optparse.parse!
 
-      # We have use the @no_args var because OptParse consumes ARGV as it parses
-      # so we have to check the value of ARGV at the begining of the method,
+      # we have use the @no_args var because optparse consumes argv as it parses
+      # so we have to check the value of argv at the begining of the method,
       # let the options be set, then output usage.
       puts optparse if @no_args
 
       # merge in the options that we read from the file
       @options = @options_from_file.merge(@options)
       # merge in defaults
-      @options = @defaults.merge(@options)
+      @options = DEFAULTS.merge(@options)
 
       if @options[:type] !~ /(pe)|(git)/
         raise ArgumentError.new("--type must be one of pe or git, not '#{@options[:type]}'")
@@ -306,15 +308,15 @@ module PuppetAcceptance
     def self.parse_options_file(options_file_path)
       options_file_path = File.expand_path(options_file_path)
       unless File.exists?(options_file_path)
-        raise ArgumentError, "Specified options file '#{options_file_path}' does not exist!"
+        raise ArgumentError, "specified options file '#{options_file_path}' does not exist!"
       end
-      # This eval will allow the specified options file to have access to our
-      #  scope.  It is important that the variable 'options_file_path' is
+      # this eval will allow the specified options file to have access to our
+      #  scope.  it is important that the variable 'options_file_path' is
       #  accessible, because some existing options files (e.g. puppetdb) rely on
       #  that variable to determine their own location (for use in 'require's, etc.)
       result = eval(File.read(options_file_path))
       unless result.is_a? Hash
-        raise ArgumentError, "Options file '#{options_file_path}' must return a hash!"
+        raise ArgumentError, "options file '#{options_file_path}' must return a hash!"
       end
 
       result
